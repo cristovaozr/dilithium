efa5cbc6b2928ce0bcfe675eedf700e38c5d7bb9
Cristóvão Zuppardo Rufino <cristovaozr@gmail.com>
2024-06-13 09:51:38 -0300

    Add new cpucycles() function

--- ref/test/cpucycles.c
+++ ref/test/cpucycles.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "cpucycles.h"
+#include <time.h>
 
 uint64_t cpucycles_overhead(void) {
   uint64_t t0, t1, overhead = -1LL;
@@ -15,3 +16,19 @@ uint64_t cpucycles_overhead(void) {
 
   return overhead;
 }
+
+#if 1
+
+inline uint64_t cpucycles(void)
+{
+  static uint64_t first_time = 0;
+  struct timespec tp;
+
+  clock_gettime(CLOCK_REALTIME, &tp);
+  if (first_time == 0) {
+    first_time = tp.tv_sec;
+  }
+  return (tp.tv_sec - first_time) * 1000000000 + tp.tv_nsec;
+}
+
+#endif
